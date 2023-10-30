3e00127bee98cda45faa71bb6157c953c6c35401
Rafael Hayashi <rafaelhayashi8@gmail.com>
2023-10-29 21:09:07 -0300

    test: teste para o método de chamar todos os personagens da API

---
+++ test/src/data/services/hp_service_test.dart
@@ -0,0 +1,12 @@
+import 'package:dio/dio.dart';
+import 'package:flutter_harry_potter/src/data/models/character.dart';
+import 'package:flutter_harry_potter/src/data/services/hp_service.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  final service = HpService(Dio());
+  test('getAllCharacters', () async {
+    final List<Character> list = await service.getAllCharacters();
+    expect(list, isNotEmpty);
+  });
+}
