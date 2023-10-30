2bef450d77260e4e0ef4c62d19974ec65f7f691c
Rafael Hayashi <rafaelhayashi8@gmail.com>
2023-10-30 17:41:55 -0300

    feat: adicionando responsavidade inicial do home pelo bloc

--- lib/src/ui/pages/home_page.dart
+++ lib/src/ui/pages/home_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_harry_potter/src/interactors/bloc/hp_bloc.dart';
+import 'package:flutter_modular/flutter_modular.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -10,11 +12,27 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Home'),
+    final bloc = context.watch<HpBloc>((bloc) => bloc.stream);
+    HpState state = bloc.state;
+    bool isSuccess = state is HpSuccess;
+    bool isLoading = state is HpLoading;
+    bool isError = state is HpError;
+    return SafeArea(
+      child: Scaffold(
+        body: Column(
+          children: [
+            ElevatedButton(
+              onPressed: () async {
+                bloc.add(GetAllCharacters());
+              },
+              child: const Text('Carregar'),
+            ),
+            if (isSuccess) const Text('sucessss'),
+            if (isLoading) Center(child: const CircularProgressIndicator()),
+            if (isError) const Text('sucessss'),
+          ],
+        ),
       ),
-      body: Container(),
     );
   }
 }
