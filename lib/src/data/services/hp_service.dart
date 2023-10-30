64f25ef12957260a5398d59ec2ddc2cbfb5b8cc8
Rafael Hayashi <rafaelhayashi8@gmail.com>
2023-10-30 17:39:57 -0300

    refactor: tratando service

--- lib/src/data/services/hp_service.dart
+++ lib/src/data/services/hp_service.dart
@@ -3,16 +3,19 @@ import 'package:flutter_harry_potter/src/data/models/character.dart';
 import 'package:flutter_harry_potter/src/interactors/services/hp_service.dart';
 
 class HpService implements IHpService {
-  final Dio _dio;
-
-  HpService(this._dio);
+  final _dio = Dio();
 
   @override
   Future<List<Character>> getAllCharacters() async {
-    const url = 'https://hp-api.onrender.com/api/characters';
-    final response = await _dio.get(url);
-    var jsonToList = response.data as List;
-    var list = jsonToList.map((e) => Character.fromMap(e)).toList();
-    return list;
+    try {
+      const url = 'https://hp-api.onrender.com/api/characters';
+      final response = await _dio.get(url);
+      var jsonToList = response.data as List;
+      var list = jsonToList.map((e) => Character.fromMap(e)).toList();
+      list.sort((a, b) => a.name.compareTo(b.name));
+      return list;
+    } catch (e) {
+      return [];
+    }
   }
 }
