c7700109a77ea67efef1c018e1c4d68e6e0e5c1a
Rafael Hayashi <rafaelhayashi8@gmail.com>
2023-10-31 17:49:44 -0300

    feat: finalizando page de descricao de personagens

--- lib/src/ui/pages/character_page.dart
+++ lib/src/ui/pages/character_page.dart
@@ -12,7 +12,63 @@ class CharacterPage extends StatelessWidget {
       appBar: AppBar(
         title: Text(character.name),
       ),
-      body: Container(),
+      body: Center(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(10),
+              child: Text(
+                character.name,
+                style: const TextStyle(
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            Image.network(
+              character.image!,
+              width: 300,
+              height: 350,
+              errorBuilder: (context, error, stackTrace) {
+                return const Image(image: AssetImage('assets/images/default_image.jpg'));
+              },
+              fit: BoxFit.contain,
+            ),
+            const SizedBox(height: 8),
+            Text(
+              'Actor: ${character.actor}',
+              style: const TextStyle(
+                fontSize: 16,
+              ),
+            ),
+            Text(
+              'Birth Date: ${character.dateOfBirth}',
+              style: const TextStyle(
+                fontSize: 16,
+              ),
+            ),
+            Text(
+              'Hair Color: ${character.hairColour}',
+              style: const TextStyle(
+                fontSize: 16,
+              ),
+            ),
+            Text(
+              'Eye Color: ${character.eyeColour}',
+              style: const TextStyle(
+                fontSize: 16,
+              ),
+            ),
+            Text(
+              'House: ${character.house}',
+              style: const TextStyle(
+                fontSize: 16,
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
