efdb7427b7081aa94df926b4edd938ec873fd051
Rafael Hayashi <rafaelhayashi8@gmail.com>
2023-10-31 01:00:20 -0300

    chore: alterando de modular para bloc para injecao

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_harry_potter/src/app_module.dart';
 import 'package:flutter_harry_potter/src/app_widget.dart';
-import 'package:flutter_modular/flutter_modular.dart';
 
 void main() {
-  runApp(
-    ModularApp(
-      module: AppModule(),
-      child: const AppWidget(),
-    ),
-  );
+  runApp(const AppWidget());
 }
--- lib/src/app_widget.dart
+++ lib/src/app_widget.dart
@@ -1,20 +1,34 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_modular/flutter_modular.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_harry_potter/src/data/models/character.dart';
+import 'package:flutter_harry_potter/src/data/services/hp_service.dart';
+import 'package:flutter_harry_potter/src/interactors/bloc/hp_bloc.dart';
+import 'package:flutter_harry_potter/src/ui/pages/character_page.dart';
+import 'package:flutter_harry_potter/src/ui/pages/home_page.dart';
 
-class AppWidget extends StatefulWidget {
+class AppWidget extends StatelessWidget {
   const AppWidget({super.key});
 
   @override
-  State<AppWidget> createState() => _AppWidgetState();
-}
-
-class _AppWidgetState extends State<AppWidget> {
-  @override
   Widget build(BuildContext context) {
-    Modular.setInitialRoute('/');
-    return MaterialApp.router(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      routerConfig: Modular.routerConfig,
+      // home: const HomePage(),
+      routes: {
+        '/': (_) => BlocProvider(
+              create: (_) => HpBloc(HpService())..add(GetAllCharactersEvent()),
+              child: const HomePage(),
+            ),
+      },
+      onGenerateRoute: ((settings) {
+        if (settings.name == '/character') {
+          final args = settings.arguments as Character;
+          return MaterialPageRoute(builder: (context) {
+            return CharacterPage(character: args);
+          });
+        }
+        return null;
+      }),
     );
   }
 }
