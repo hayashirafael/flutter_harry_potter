39a9599914e55219717c4d22b1db89781bbf60fd
Rafael Hayashi <rafaelhayashi8@gmail.com>
2023-10-31 01:07:55 -0300

    refactor: alterando estrutura para bloc builder

--- lib/src/ui/pages/home_page.dart
+++ lib/src/ui/pages/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_harry_potter/src/interactors/bloc/hp_bloc.dart';
-import 'package:flutter_modular/flutter_modular.dart';
+import 'package:flutter_harry_potter/src/ui/widgets/error_widget.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -12,26 +13,36 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    final bloc = context.watch<HpBloc>((bloc) => bloc.stream);
-    HpState state = bloc.state;
-    bool isSuccess = state is HpSuccess;
-    bool isLoading = state is HpLoading;
-    bool isError = state is HpError;
-    return SafeArea(
-      child: Scaffold(
-        body: Column(
-          children: [
-            ElevatedButton(
-              onPressed: () async {
-                bloc.add(GetAllCharacters());
+    return Scaffold(
+      appBar: AppBar(title: const Text('Home')),
+      body: BlocBuilder<HpBloc, HpState>(
+        builder: (context, state) {
+          if (state is HpLoading) {
+            return const Center(child: CircularProgressIndicator());
+          }
+          if (state is HpError) {
+            return AppErrorWidget(errorMessage: state.message);
+          }
+          if (state is HpSuccess) {
+            return ListView.builder(
+              itemCount: state.list.length,
+              itemBuilder: (context, index) {
+                final character = state.list[index];
+                return GestureDetector(
+                  onTap: () => Navigator.of(context).pushNamed('/character', arguments: character),
+                  child: ListTile(
+                    // leading: CircleAvatar(
+                    //   child: character.image != '' ? Image.network(character.image!) : const SizedBox.shrink(),
+                    // ),
+                    title: Text(character.name),
+                    subtitle: Text(character.house),
+                  ),
+                );
               },
-              child: const Text('Carregar'),
-            ),
-            if (isSuccess) const Text('sucessss'),
-            if (isLoading) Center(child: const CircularProgressIndicator()),
-            if (isError) const Text('sucessss'),
-          ],
-        ),
+            );
+          }
+          return const SizedBox.shrink();
+        },
       ),
     );
   }
